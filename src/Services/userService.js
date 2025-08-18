52a19649f19f2d37cf55cf510f7e95c428b8a910
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-08-18 09:59:26 -0300

    limpeza

--- src/Services/userService.js
+++ src/Services/userService.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import repoUser from "../repositories/repoUser.js";
 
 async function CadastroUser(user_email, user_password, user_name, user_cel_phone, endereco) {
@@ -44,50 +44,3 @@ async function EditarUser(id_user, user_name, user_email, endereco, user_passwor
 
 
 export default { CadastroUser, LoginUser, ProfileUser, EditarUser }
-=======
-import repoUser from "../repositories/repoUser.js";
-
-async function CadastroUser(user_email, user_password, user_name, user_cel_phone, endereco) {
-
-    const hashPassword = await bcrypt.hash(user_password, 10);
-    const admin = await repoUser.CadastroUser(user_email, hashPassword, user_name, user_cel_phone, endereco);
-
-    admin.token = jwt.CreateToken(admin.id_user);
-
-    return admin;
-}
-async function LoginUser(user_email, user_password) {
-
-    const userData = await repoUser.LoginUser(user_email);
-
-    if (!userData)
-        return null;
-    else {
-        if (await bcrypt.compare(user_password, userData.user_password)) {
-            delete userData.user_password;
-
-            userData.token = jwt.CreateToken(userData.id_user);
-
-            return userData;
-        } else
-            return [];
-    }
-}
-async function ProfileUser(id_user) {
-
-    const userProfile = await repoUser.ProfileUser(id_user);
-
-    return userProfile;
-}
-
-async function EditarUser(id_user, user_name, user_email, endereco, user_password, user_cel_phone) {
-
-    const usuario = await repoUser.EditarUser(id_user, user_name, user_email,
-        endereco, user_password, user_cel_phone);
-
-    return usuario;
-}
-
-
-export default { CadastroUser, LoginUser, ProfileUser, EditarUser }
->>>>>>> a974904df9ae90895ebc61cb52b048723a0433a2
--- src/database/db.js
+++ src/database/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pg from 'pg'
 const { Pool } = pg
 
@@ -11,17 +10,3 @@ const pool = new Pool({
 });
 
 export default pool;
-=======
-import pg from 'pg'
-const { Pool } = pg
-
-const pool = new Pool({
-     user: 'postgres.yulykztzhmoxfztykeop',     
-     host: 'aws-0-sa-east-1.pooler.supabase.com',     
-     database: 'postgres',
-     password: 'Estacio091@@',
-     port: 6543,
-});
-
-export default pool;
->>>>>>> a974904df9ae90895ebc61cb52b048723a0433a2

472a3a1493b05daf45f4d70f7e88218bb2adf980
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-08-18 09:49:06 -0300

    corrigido

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+
+
+const router = Router();
+
+// Rotas Usuários
+
+// Rotas Admin
+
+// Rotas Posts
+
+export default router;
+import { Router } from "express";
+
+
+const router = Router();
+
+// Rotas Usuários
+
+// Rotas Admin
+
+// Rotas Posts
+
+export default router;

8f3d3c5abfc0d286378d485ee01b5cf2b85a9219
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-08-23 09:10:23 -0300

    text messages

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -3,7 +3,7 @@ import messageService from "../Services/messageService.js";
 
 const postMessage = async (req, res) => {
   try {
-    const { sender_id, receiver_id, message_text } = req.body;
+    const { sender_id, receiver_id, message_text} = req.body;
     const message = await messageService.createMessage({ sender_id, receiver_id, message_text });
     res.status(201).json(message);
   } catch (error) {
--- src/repositories/repoMessage.js
+++ src/repositories/repoMessage.js
@@ -2,15 +2,30 @@ import pool from "../database/db.js"
 
 
 async function SendMessages({ sender_id, receiver_id, message_text }) {
-
-    const query = `INSERT INTO anama_messages (sender_id, receiver_id, text)
+  const query = `
+    INSERT INTO anama_messages (sender_id, receiver_id, message_text)
     VALUES ($1, $2, $3)
     RETURNING *;
   `;
-    const values = [sender_id, receiver_id, message_text];
+  const values = [sender_id, receiver_id, message_text];
 
+  try {
     const result = await pool.query(query, values);
     return result.rows[0];
+  } catch (error) {
+    console.error('Erro ao enviar mensagem:', error);
+    throw new Error('Não foi possível enviar a mensagem');
+  }
+
 }
+
+//     const query = `INSERT INTO anama_messages (sender_id, receiver_id, text)
+//     VALUES ($1, $2, $3)
+//     RETURNING *;
+//   `;
+//     const values = [sender_id, receiver_id, message_text];
+
+//     const result = await pool.query(query, values);
+//     return result.rows[0];
 
 export default {SendMessages};

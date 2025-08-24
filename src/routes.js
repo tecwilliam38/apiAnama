2dd6edecf7da21eb835b9da3287bed64c175ff37
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-08-24 20:14:14 -0300

    rota

--- src/routes.js
+++ src/routes.js
@@ -23,5 +23,10 @@ router.post('/friends/contact', jwt.ValidateToken, userController.addFriendByCon
 router.post('/messages/send', jwt.ValidateToken, messageController.postMessage);
 router.get('/messages/get', jwt.ValidateToken, messageController.getMessages);
 router.get('/messages/:userId',jwt.ValidateToken,userController.listMyFriendsHandler);
+router.post('/messages/put', jwt.ValidateToken, messageController.sendMessageHandler);
+// Buscar conversa com um amigo
+router.get('/messages/get/:friend_id', jwt.ValidateToken, getConversationHandler);
+
+
 
 export default router;

57871853662dbb81b4fdef3cb4daaca35f39fc8b
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-09-29 08:49:03 -0300

    clean

--- src/controllers/agendaController.js
+++ src/controllers/agendaController.js
@@ -14,19 +14,13 @@ async function InsertAgenda(req, res) {
     }
 }
 
-// const { dt_start, dt_end } = req.query;
-
-// Converte para null se estiver vazio
-// dt_start = dt_start || null;
-// dt_end = dt_end || null;
 async function ListarServicos(req, res) {
 
     const id_user = req.user.id_user;
     let { dt_start, dt_end } = req.body;
-
-    // if (dt_start && dt_start.length === 10) {
-    //     dt_start = dt_start + 'T00:00:00';
-    // }
+    if (dt_start && dt_start.length === 10) {
+        dt_start = dt_start + 'T00:00:00';
+    }
 
     if (dt_end && dt_end.length === 10) {
         dt_end = dt_end + 'T23:59:59';
